d4c715f346eb51e309e77f9ec8ad84fcaf99731a
Ahmedna <elbechia@im2ag-216-03.u-ga.fr>
2026-03-25 08:34:43 +0100

    creating project structure using maven

---
+++ ndarray-lib/src/main/java/com/im2ag/m1info/ndarray/App.java
@@ -0,0 +1,10 @@
+package com.im2ag.m1info.ndarray;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
