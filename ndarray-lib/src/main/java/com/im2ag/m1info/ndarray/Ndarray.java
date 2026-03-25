a979ad45bf08188390bf5859bf442d5ff1029312
benoit sion <pheonix120@hotmail.com>
2026-03-25 09:31:22 +0100

    ajout class attributes et squelette des fonctions

--- ndarray-lib/src/main/java/com/im2ag/m1info/ndarray/Ndarray.java
+++ ndarray-lib/src/main/java/com/im2ag/m1info/ndarray/Ndarray.java
@@ -1,12 +1,38 @@
+package com.im2ag.m1info.ndarray;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Ndarray {
     List l;
     int size;
+    int ndim;
+    int[] shape;
+    String dtype; //Voir faire une enum
+
 
     public Ndarray() {
         l = new ArrayList<>();
+    }
+
+    public Ndarray array(int[] array){
+
+        return new Ndarray();
+    }
+
+    public Ndarray zeros(){
+
+        return new Ndarray();
+    }
+
+    public Ndarray arange(int nb){
+
+        return new Ndarray();
+    }
+
+    public void reshape(int axe1, int axe2){
+
+
     }
 
     List getList() {
