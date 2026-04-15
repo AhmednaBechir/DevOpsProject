80248e93445cc5b7370a25f4fd4ed885f3096c47
Victor Schwartzmann <victor.schwartzmann@etu.univ-grenoble-alpes.fr>
2026-04-15 08:54:17 +0200

    Corrections test : convention Java

--- ndarray-lib/src/test/java/com/im2ag/m1info/ndarray/NumjaTest.java
+++ ndarray-lib/src/test/java/com/im2ag/m1info/ndarray/NumjaTest.java
@@ -8,16 +8,15 @@ class NumjaTest {
 
 	@Test
 	void arrayTest() {
-		double data[] = {0,1,2};
-		int[] shape = {3};
+		double[] data = {0,1,2};
 		Ndarray ndarray_test = Numja.array(data);
 		assertEquals(3, ndarray_test.getSize(), "arrayTest");
 	}
 	
 	@Test
 	public void initializationTest() {
-		double data[][] = {{1,2,3}, {4,5,6}};
-		int shape[] = {2, 3};
+		double[][] data = {{1,2,3}, {4,5,6}};
+		int[] shape = {2, 3};
 		Ndarray ndarray_test = Numja.array(data);
 		for (int i = 0; i < 2; i++) {
 			assertEquals(shape[i], ndarray_test.shape[i], "Numja initializationTest: shape");
@@ -33,7 +32,7 @@ class NumjaTest {
 	
 	@Test
 	public void zerosTest() {
-		int shape[] = {3};
+		int[] shape = {3};
 		Ndarray zeros_test1 = Numja.zeros(shape);
 		assertEquals(1, zeros_test1.ndim, "Numja zerosTest: ndim");
 		assertEquals(3, zeros_test1.shape[0], "Numja zerosTest: shape[0]");
@@ -42,9 +41,9 @@ class NumjaTest {
 	
 	@Test
 	public void addTest() {
-		double data1[][] = {{0,1,2}, {3,4,5}};
-		double data2[][] = {{0,1,2}, {3,4,5}};
-		double expected_result[][] = {{0,2,4}, {6,8,10}};
+		double[][] data1 = {{0,1,2}, {3,4,5}};
+		double[][] data2 = {{0,1,2}, {3,4,5}};
+		double[][] expected_result = {{0,2,4}, {6,8,10}};
 		
 		Ndarray array_1 = Numja.array(data1);
 		Ndarray array_2 = Numja.array(data2);
@@ -59,9 +58,9 @@ class NumjaTest {
 	
 	@Test
 	public void subTest() {
-		double data1[] = {0,1,2};
-		double data2[] = {2.,-1.,4};
-		double expected_result[] = {-2, 2, -2};
+		double[] data1 = {0,1,2};
+		double[] data2 = {2.,-1.,4};
+		double[] expected_result = {-2, 2, -2};
 		
 		Ndarray array_1 = Numja.array(data1);
 		Ndarray array_2 = Numja.array(data2);
@@ -76,11 +75,11 @@ class NumjaTest {
 	
 	@Test
 	public void mulTest() {
-		double M1[][] = {{1,2}, {3,4}, {5,6}};
-		double M2[][] = {{1,2,3}, {4,5,6}};
+		double[][] M1 = {{1,2}, {3,4}, {5,6}};
+		double[][] M2 = {{1,2,3}, {4,5,6}};
 		
-		double expected_result_M1_M2[][] = {{22, 28}, {49, 64}};
-		double expected_result_M2_M1[][] = {{9, 12, 15}, {19, 26, 33}, {29, 40, 51}};
+		double[][] expected_result_M1_M2 = {{22, 28}, {49, 64}};
+		double[][] expected_result_M2_M1 = {{9, 12, 15}, {19, 26, 33}, {29, 40, 51}};
 		
 		Ndarray array_1 = Numja.array(M1);
 		Ndarray array_2 = Numja.array(M2);
@@ -108,15 +107,15 @@ class NumjaTest {
 	
 	@Test
 	public void getSizeTest() {
-		double data1[] = {0,1,2};
+		double[] data1 = {0,1,2};
 		Ndarray ndarray_test = Numja.array(data1);
 		assertEquals(3, ndarray_test.getSize(), "Numja getSizeTest: length 2");
 		
-		double data2[] = {0,1};
+		double[] data2 = {0,1};
 		ndarray_test = Numja.array(data2);
 		assertEquals(2, ndarray_test.getSize(), "Numja getSizeTest: length 3");
 		
-		double data3[][] = {{0,1,2}, {3,4,5}};
+		double[][] data3 = {{0,1,2}, {3,4,5}};
 		ndarray_test = Numja.array(data3);
 		assertEquals(6, ndarray_test.getSize(), "Numja getSizeTest: length 6");
 		
