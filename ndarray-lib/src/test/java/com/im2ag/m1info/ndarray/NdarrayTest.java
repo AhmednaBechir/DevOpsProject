48b5f64eb8bb927205dad3560bd96e0c32b341fc
Victor Schwartzmann <victor.schwartzmann@etu.univ-grenoble-alpes.fr>
2026-04-22 10:37:21 +0200

    Ajout tests print + mul

--- ndarray-lib/src/test/java/com/im2ag/m1info/ndarray/NdarrayTest.java
+++ ndarray-lib/src/test/java/com/im2ag/m1info/ndarray/NdarrayTest.java
@@ -58,7 +58,7 @@ class NdarrayTest {
 	}
 	
 	@Test
-	public void mulTest() {
+	public void mulTest1() {
 		double data1[][] = {{1,2,3}};
 		double data2[][] = {{1}, {2}, {3}};
 		
@@ -67,8 +67,25 @@ class NdarrayTest {
 		
 		array_1.mul(array_2);
 		
-		assertEquals(14, array_1.data[0][0]);
+		assertEquals(14, array_1.data[0][0], "mulTest1");
+	}
+	
+	@Test
+	public void mulTest2() {
+		double data1[][] = {{1,2,3},{4,5,6}};
+		double data2[][] = {{1,2}, {3,4}, {5,6}};
+		double[][] expected_result = {{22, 28}, {49, 64}};
 		
+		Ndarray array_1 = Numja.array(data1);
+		Ndarray array_2 = Numja.array(data2);
+		
+		array_1.mul(array_2);
+		
+		for (int i = 0; i < array_1.shape[0]; i++) {
+			for (int j = 0; j < array_1.shape[1]; j++) {
+				assertEquals(expected_result[i][j], array_1.data[i][j], "mulTest2");
+			}
+		}
 	}
 	
 	@Test
@@ -82,14 +99,64 @@ class NdarrayTest {
 		int shape2[] = {2,1};
 		ndarray_test = new Ndarray(data2, 2, 1, shape2);
 		assertEquals(2, ndarray_test.getSize(), "getSizeTest: length 2");
+	}
+	
+	@Test
+	public void printTest1() {
+		double[][] data = {{1,2,3}, {4,5,6}};
 		
+		Ndarray test = Numja.array(data);
+		String result = test.print();
 		
+		assertEquals("[[1.0, 2.0, 3.0]\n[4.0, 5.0, 6.0]]", result, "printTest1");
+	}
+	
+	@Test
+	public void printTest2() {
+		double[] data = {1,2,3,4,5,6};
+		
+		Ndarray test = Numja.array(data);
+		String result = test.print();
+		
+		assertEquals("[1.0, 2.0, 3.0, 4.0, 5.0, 6.0]", result, "printTest2");
+	}
+	
+	@Test
+	public void printZerosTest1() {
+		int[] shape = {3,2};
+		Ndarray test = Numja.zeros(shape);
+		String result = test.print();
+		
+		assertEquals("[[0.0, 0.0]\n[0.0, 0.0]\n[0.0, 0.0]]", result, "printZerosTest1");
+	}
+	
+	@Test
+	public void printZerosTest2() {
+		int[] shape = {3};
+		Ndarray test = Numja.zeros(shape);
+		String result = test.print();
+		
+		assertEquals("[0.0, 0.0, 0.0]", result, "printZerosTest2");
+	}
+	
+	@Test
+	public void printZerosTest3() {
+		int[] shape = {0};
+		Ndarray test = Numja.zeros(shape);
+		String result = test.print();
+		System.out.println(result);
+		assertEquals("", result, "printZerosTest3");
+	}
+	
+	@Test
+	public void printZerosTest4() {
+		int[] shape = {0,0};
+		Ndarray test = Numja.zeros(shape);
+		String result = test.print();
+		
+		assertEquals("", result, "printZerosTest4");
 	}
 	
 	
-	
-	
-	
-	
-	
 }
+ 
